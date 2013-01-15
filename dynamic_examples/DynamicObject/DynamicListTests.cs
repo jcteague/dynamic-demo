429a46662f61db252a20c985ba128fbd6ed39cd6
John Teague <jcteague@gmail.com>
2013-01-15 16:37:36 -0600

    ListBag done

---
+++ dynamic_examples/DynamicObject/DynamicListTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace dynamic_examples.DynamicObjectSamples
+{
+    [TestFixture]
+    public class DynamicListTests
+    {
+        [Test]
+        public void can_get_and_set_by_name()
+        {
+            dynamic lb = new ListBag();
+            lb.Name = "John";
+            Assert.AreEqual(lb.Name,"John");
+        }
+
+        [Test]
+        public void can_get_by_index()
+        {
+            dynamic lb = new ListBag();
+            lb.Name = "John";
+            Assert.That(lb[0], Is.EqualTo("John"));
+            
+        }
+
+        [Test]
+        public void can_create_new_bag_with_list()
+        {
+            dynamic lb = new ListBag();
+            lb <<= new {Name = "John", Email="email"};
+            Assert.That(lb.Name, Is.EqualTo("John"));
+            Assert.That(lb.Email, Is.EqualTo("email"));
+        }
+    }
+}
---
+++ dynamic_examples/DynamicObject/ListBag.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Linq.Expressions;
+
+namespace dynamic_examples.DynamicObjectSamples
+{
+    public class ListBag : DynamicObject
+    {
+        IDictionary<string,object> _dict = new Dictionary<string,object>();
+        IList<object> _list = new List<object>();
+        public ListBag(){}
+        private ListBag(IDictionary<string, object> dict, IList<object> value_list)
+        {
+            
+        }
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            AddMember(binder.Name, value);
+            return true;
+        }
+
+        void AddMember(string name, object value)
+        {
+            _dict[name] = value;
+            _list.Add(value);
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            
+            return _dict.TryGetValue(binder.Name, out result);
+            
+        }
+        public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object result)
+        {
+            var i = (int) indexes[0];
+            result = _list[i];
+            return true;
+        }
+        public override bool TryBinaryOperation(BinaryOperationBinder binder, object arg, out object result) {
+            if(binder.Operation == ExpressionType.LeftShiftAssign)
+            {
+                var copy = new ListBag(_dict, _list);
+                copy.AddMembersFromObject(arg);
+                result = copy;
+                return true;
+            }
+            result = null;
+            return false;
+        }
+
+        void AddMembersFromObject(object o)
+        {
+            var properties = TypeDescriptor.GetProperties(o);
+            foreach (PropertyDescriptor prop in properties)
+            {
+                AddMember(prop.Name,prop.GetValue(o));
+            }
+
+        }
+        
+    }
+}
