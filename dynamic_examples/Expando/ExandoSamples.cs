c2ea8255b841eb003825f0a1799f140e6de6789c
John Teague <jcteague@gmail.com>
2013-01-15 14:54:30 -0600

    started expando

---
+++ dynamic_examples/Expando/ExandoSamples.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace dynamic_examples.Expando
+{
+    [TestFixture]
+    public class ExandoSamples
+    {
+        [Test]
+        public void adding_properties()
+        {
+            dynamic eo = new ExandoObject();
+        }
+    }
+}
---
+++ dynamic_examples/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dynamic_examples {
+    
+    class Program {
+        static void Main(string[] args)
+        {
+            IEnumerable<dynamic> list = new List<dynamic>();
+        }
+        public static void do_something(IEnumerable<dynamic> dyn_list)
+        {
+            
+        }
+    }
+}
