c5d2f119fb4278a3a7fb672d55faaa5fabcede1d
John Teague <jcteague@gmail.com>
2013-01-15 15:15:16 -0600

    expando event handlers

---
+++ dynamic_examples/Expando/ExpandoPropertyNotfiier.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Dynamic;
+using NUnit.Framework;
+
+namespace dynamic_examples.Expando
+{
+    [TestFixture]
+    public class ExpandoPropertyNotfiier
+    {
+        void EventHandler1(object sender, EventArgs args)
+        {
+            Console.WriteLine("Event Handler 1 Activated");
+        }
+        
+        [Test]
+        public void dynamic_event_handlers()
+        {
+            dynamic eo = new ExpandoObject();
+            eo.evt = null;
+            eo.evt += new EventHandler(EventHandler1);
+            eo.evt += new EventHandler((sender, args) =>
+                                           {
+                                               Console.WriteLine("Inline EventHandler");
+                                           });
+
+            EventHandler e = eo.evt;
+            if(e!=null)
+            {
+                e(eo, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ dynamic_examples/Expando/ExpandoSamples.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Dynamic;
+using NUnit.Framework;
+
+namespace dynamic_examples.Expando
+{
+    [TestFixture]
+    public class ExpandoSamples
+    {
+        dynamic eo = new ExpandoObject();
+        
+        [SetUp]
+        public void SetUp()
+        {
+            
+            eo.FirstName = "Mark";
+            eo.LastName = "Wahlburg";
+            eo.CatchPhrase = "Say Hi to your Mother for me.";
+        }
+        [Test]
+        public void adding_properties()
+        {
+
+            eo.FirstName = "Mark";
+            eo.LastName = "Wahlburg";
+            Console.WriteLine("Hi, I'm {0} {1}", eo.FirstName, eo.LastName);
+        }
+
+        [Test]
+        public void add_a_method()
+        {
+            eo.FirstName = "Mark";
+            eo.LastName = "Wahlburg";
+            eo.CatchPhrase = "Say Hi to your Mother for me.";
+            eo.FullName = eo.FirstName + " " + eo.LastName;
+            eo.Greet = new Func<string, string>((string person) => String.Format("Hi {0}, I'm {1}. {2}", person,eo.FullName, eo.CatchPhrase));
+            Console.WriteLine(eo.Greet("Andy"));
+        }
+    }
+}
